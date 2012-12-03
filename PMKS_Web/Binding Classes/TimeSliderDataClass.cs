e6c3bf5efb33d61d0e87a38f013e748d38ad59b9
ak22546 <ak22546@me-campbell02.austin.utexas.edu>
2012-12-03 16:35:45 -0600

    Added more logic to timeslider: -created binding class between slider and mainviewer
    I am commiting the logic at its reasonable breakpoint, in case if I am stuck or lost at something in future

---
+++ PMKS_Web/Binding Classes/TimeSliderDataClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace Binding_Classes
+{
+    public class TimeSliderDataClass: INotifyPropertyChanged
+    {
+        private double _val;
+        public double Val
+        {
+            get 
+            {
+                return _val;
+            }
+            set 
+            {
+                if (_val != value)
+                {
+                    _val = value;
+                    RaisePropertyChanged("Val");
+                }
+            }
+        }
+
+        private int _numberOfPoints;
+        public int NumberOfPoints
+        {
+            get
+            {
+                return _numberOfPoints;
+            }
+            set
+            {
+                if (_numberOfPoints != value)
+                {
+                    _numberOfPoints = value;
+                    RaisePropertyChanged("NumberOfPoints");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void RaisePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        } 
+    }
+}
