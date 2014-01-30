8eadfd39380f961f9b6730e01b3192a2ce92173a
Matt Campbell <mattica@gmail.com>
2014-01-29 16:22:56 -0800

    added enumerator and examples...

---
+++ PlanarMechanismSimulator/Enumerators.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanarMechanismSimulator
+{
+    /// <summary>
+    /// The mechanism will have a characteristic repeat cycle.
+    /// </summary>
+    public enum CycleTypes
+    {
+        LessThanFullCycle = -1,
+        OneCycle = 0,
+        MoreThanOneCycle = 1
+    }
+
+    /// <summary>
+    /// the accepted joint types used in this software
+    /// </summary>
+    public enum JointTypes
+    {
+        R,
+        P,
+        RP,
+        G
+        // non-slip roll, like rack and pinion - although this challenges the 2 DOF nature of just gear teeth
+        // cabling or belt or chain
+    };
+
+    internal enum KnownState
+    {
+        Unknown,
+        Partially,
+        Fully
+    };
+
+
+    internal enum PositionAnalysisResults { NoSolvableDyadFound, Normal, InvalidPosition, BranchingProbable }
+}
